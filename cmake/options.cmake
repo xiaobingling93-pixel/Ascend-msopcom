18be87223f4cc8675f88bc656ec075b9a03824ca
mengguangxin <mgx0018@163.com>
2026-02-02 10:19:21 +0800

    【feature】增加debug编译功能，支持VSCode断点调试
    Co-authored-by: mengguangxin<mgx0018@163.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !26 merge dev0126 into master
    
    【feature】增加debug编译功能，支持VSCode断点调试
    
    Created-by: mengguangxin
    Commit-by: mengguangxin
    Merged-by: ascend-robot
    Description: ### 1. 修改描述
    - **修改原因：**  为了支撑在VSCode上进行单步断点调试，需要编译debug版本，目前工程不具备此能力
    - **修改方案：**  修改CMake，增加debug编译功能，这样执行编译：cmake ../cmake/ -DCMAKE_BUILD_TYPE=Debug , 可编译出debug版本
    - **修改内容：**  增加CMAKE_BUILD_TYPE的判断，对Debug和Release两种场景分别设置不同的编译开关
    - [ ] **涉及代码双合**(贴上另一个PR链接)：https://gitcode.com/Ascend/mssanitizer/pull/26
    ----
    
    ### 2. 功能验证
    - [ ] **功能自验截图**（请确保不体现个人信息）  开启后，可直接断点
    ![image.png](https://raw.gitcode.com/user-images/assets/8858748/a10caa94-02e5-4a0b-afcc-bf873b7770fb/image.png 'image.png')
    - [ ] **冒烟是否通过**  通过
    ----
    
    ### 3. 代码检视
    - **要求：**
      - 合入功能代码大于 200 行，需要sig会议申报代码检视议题，并在PR中标注会议。
      - committer评估是否需要在sig会议进行代码检视。
      - 参与检视的committer人员名单与检视时间。
      - 大于 1000 行代码原则上不允许合入，需进行备案。
    - [ ] **是否经过代码检视**  是
    - [ ] **是否具备UT测试用例看护** 不涉及
    - [ ] **是否需要在sig会议中进行代码检视** 否
    - **检视committer人员名单与检视时间：**
    ----
    ### 4. 资料修改自检
    - **资料修改：**    不涉及
    ----
    
    See merge request: Ascend/msopcom!26

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -5,16 +5,25 @@ add_compile_options("-std=c++11")
 add_compile_options("-fPIC")
 add_compile_options("-fstack-protector-all")
 add_compile_options("-Wall")
-add_compile_options("-D_FORTIFY_SOURCE=2")
+
 add_compile_options("-fno-strict-aliasing")
-add_compile_options("-O2")
+
+# 根据CMAKE_BUILD_TYPE选择编译选项（从父项目继承）
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    add_compile_options("-O0")
+else()
+    add_compile_options("-O2")
+    add_compile_options("-D_FORTIFY_SOURCE=2")
+endif()
 
 if(NOT CMAKE_VERSION VERSION_LESS "3.13")
     add_link_options ("-Wl,-z,now")
     add_link_options ("-Wl,-z,relro")
     add_link_options ("-Wl,-z,noexecstack")
     add_link_options ("-pie")
-    add_link_options ("-s") #strip
+    if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
+        add_link_options ("-s")  # strip symbols
+    endif()
 endif()
 
 set(CMAKE_CXX_FLAGS "-Wno-builtin-macro-redefined -U__FILE__ -D__FILE__='\"$(notdir $(abspath $<))\"'")
